db9267a82d8d756084984cb6360fe26afa145f50
rockchipgh <57034646+rockchipgh@users.noreply.github.com>
2021-04-17 23:16:57 +0530

    Add files via upload

---
+++ Rainbow Road/script.js
@@ -0,0 +1,51 @@
+a=1
+function reset() {
+    a=1
+}
+
+function red() {
+    document.getElementById(a).color = "red"
+    a=a+1
+    if(a==25){reset();}
+}
+
+function blue() {
+    document.getElementById(a).color = "blue"
+    a=a+1
+    if(a==25){reset();}
+}
+function green() {
+    document.getElementById(a).color = "green"
+    a=a+1
+    if(a==25){reset();}
+}
+function yellow() {
+    document.getElementById(a).color = "yellow"
+    a=a+1
+    if(a==25){reset();}
+}
+function pink() {
+    document.getElementById(a).color = "pink"
+    a=a+1
+    if(a==25){reset();}
+}
+function orange() {
+    document.getElementById(a).color = "orange"
+    a=a+1
+    if(a==25){reset();}
+}
+function violet() {
+    document.getElementById(a).color = "violet"
+    a=a+1
+    if(a==25){reset();}
+}
+function lime() {
+    document.getElementById(a).color = "lime"
+    a=a+1
+    if(a==25){reset();}
+}
+function white() {
+    document.getElementById(a).color = "white"
+    a=a+1
+    if(a==25){reset();}
+  }
